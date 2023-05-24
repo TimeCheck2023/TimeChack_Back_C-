07779366e653b32c927caa25669c4d8f4b47e038
kevin <slash2130kevin@gmail.com>
2023-05-24 17:30:30 -0500

    se agrego el endpoint para eliminar un miembro de una suborganización

---
+++ APIEvent/Controllers/MemberController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace APIEvent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly string connectionString;
+        public MemberController(IConfiguration config)
+        {
+            //Cadena de conexion a la DB desde el archivo appsettings.json
+            connectionString = config.GetConnectionString("CadenaSQL");
+        }
+
+        // Endpoint para eliminar un miembro
+        /// <summary>
+        /// Elimina un miembro de la tabla Miembro.
+        /// </summary>
+        /// <param name="nroDocumentoUsuario">Número de documento del usuario a eliminar.</param>
+        /// <returns>
+        /// Retorna un objeto IActionResult que indica el resultado de la operación.
+        /// Si el miembro se ha eliminado correctamente, devuelve un objeto StatusCode con el estado 200 y un mensaje de éxito.
+        /// Si hay un error interno en el servidor, devuelve un objeto StatusCode con el estado 500 y un mensaje de error.
+        /// </returns>
+        [HttpPost]
+        [Route("DeleteMember")]
+        public IActionResult EliminarMiembro(string nroDocumentoUsuario)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("USP_EliminarMiembro", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@nro_documento_usuario1", nroDocumentoUsuario);
+
+                    command.ExecuteNonQuery();
+
+                    return Ok("Miembro eliminado correctamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+    }
+}

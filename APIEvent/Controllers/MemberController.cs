34039924aca11201b4538bac6ebac807d9e7fe8d
Osorio3408 <yuliamwow@gmail.com>
2023-05-26 12:11:38 -0600

    config

--- APIEvent/Controllers/MemberController.cs
+++ APIEvent/Controllers/MemberController.cs
@@ -21,14 +21,15 @@ namespace APIEvent.Controllers
         /// Elimina un miembro de la tabla Miembro.
         /// </summary>
         /// <param name="nroDocumentoUsuario">Número de documento del usuario a eliminar.</param>
+        /// <param name="idSuborganizacion">ID de la suborganización a la que pertenece el miembro.</param>
         /// <returns>
         /// Retorna un objeto IActionResult que indica el resultado de la operación.
         /// Si el miembro se ha eliminado correctamente, devuelve un objeto StatusCode con el estado 200 y un mensaje de éxito.
         /// Si hay un error interno en el servidor, devuelve un objeto StatusCode con el estado 500 y un mensaje de error.
         /// </returns>
-        [HttpPost]
+        [HttpDelete]
         [Route("DeleteMember")]
-        public IActionResult EliminarMiembro(string nroDocumentoUsuario)
+        public IActionResult EliminarMiembro(string nroDocumentoUsuario, int idSuborganizacion)
         {
             try
             {
@@ -40,6 +41,7 @@ namespace APIEvent.Controllers
                     command.CommandType = CommandType.StoredProcedure;
 
                     command.Parameters.AddWithValue("@nro_documento_usuario1", nroDocumentoUsuario);
+                    command.Parameters.AddWithValue("@id_suborganizacion", idSuborganizacion);
 
                     command.ExecuteNonQuery();
 
@@ -51,6 +53,7 @@ namespace APIEvent.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
 
 
     }
